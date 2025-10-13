6c76e3e6d41f93fd7049c2219d976ffaa28d5599
Manqad <mr08456@st.habib.edu.pk>
2025-10-13 14:21:46 +0500

    main edited

--- main.py
+++ main.py
@@ -3,9 +3,9 @@ from train import train_patchcore
 from test import detect_anomalies
 
 # Set your paths here
-TRAIN_DATA_DIR = "data/front_flipped"  # Update this to your local training data directory
-TEST_IMAGE_PATH = "data/faulty/front_flipped.JPG"  # Update this to your local test image path
-MEMORY_BANK_PATH = "patchcore_memory_bank.pth"  # Path where memory bank will be saved/loaded
+TRAIN_DATA_DIR = "C:\\Users\\5420\\OneDrive\\Desktop\\fyp\\code\\aesthetic-fault-detection\\100MSDCF\\100MSDCF\\acpart2\\front\\good"  # Update this to your local training data directory
+TEST_IMAGE_PATH = "C:\\Users\\5420\\OneDrive\\Desktop\\fyp\\code\\aesthetic-fault-detection\\100MSDCF\\100MSDCF\\acpart2\\front\\bad\\2.JPG"  # Update this to your local test image path
+MEMORY_BANK_PATH = "C:\\Users\\5420\\OneDrive\\Desktop\\fyp\\code\\aesthetic-fault-detection\\patchcore_memory_bank.pth"  # Path where memory bank will be saved/loaded
 
 if __name__ == "__main__":
     # Train the model and save memory bank
